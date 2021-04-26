0377d9634a106bfb2504204bd249af3196da0253
kylethibbs <78558790+kylethibbs@users.noreply.github.com>
2021-04-26 17:39:50 -0400

    Add files via upload

---
+++ Thesis/diplayGraph.m
@@ -0,0 +1,48 @@
+function diplayGraph(handles, coef)   
+        
+    if ~isempty(handles.Axes.Graph.Children)
+        delete(handles.Axes.Graph.Children)
+    end
+    
+    if isempty(coef)
+        return
+    end 
+        
+    % Seperate the coefficients to get the numbers
+    coef = strsplit(coef{1},{',','\t',' '});
+    if isempty(coef{end})
+     coef = coef(1:end-1);
+    end
+    
+    
+    if strcmp(handles.ButtonGroup.Mode.SelectedObject.String,'True Value')
+        switch handles.ButtonGroup.Degree.SelectedObject.String
+            case 'Quadratic'
+                selectedSize = 3;
+            case 'Cubic'
+                selectedSize = 4;
+            case 'Quartic'
+                selectedSize = 5;
+        end
+        
+        if length(coef) ~= selectedSize
+            warndlg(['The number of coefficients is incorrect for a ', handles.ButtonGroup.Degree.SelectedObject.String,' function.'])
+            return
+        end
+    
+    end   
+
+    for i = 1:length(coef)
+        graphCoef(1,i) = str2double(coef{1,i});
+    end 
+    
+    x = -50:.01:50;
+    y = polyval(graphCoef,x);
+    
+    line('Parent',handles.Axes.Graph,'XData',x,'YData',y,'Tag','polyLine');
+
+end
+
+
+
+
---
+++ Thesis/discrim.m
@@ -0,0 +1,42 @@
+myFig = figure;
+grid on;
+hold on
+
+f1 = [1 0 -3 4];
+f2 = [1 0 -3 2];
+f3 = [1 0 -3 0];
+f4 = [1 0 -3 -2];
+f5 = [1 0 -3 -4];
+
+x = -5:.01:5;
+
+y1 = polyval(f1,x);
+y2 = polyval(f2,x);
+y3 = polyval(f3,x);
+y4 = polyval(f4,x);
+y5 = polyval(f5,x);
+
+l1 = line(x,y1,'Color','b','LineStyle','-','LineWidth',2);
+l2 = line(x,y2,'Color','r','LineStyle','--','LineWidth',2);
+l3 = line(x,y3,'Color','g','LineStyle','-.','LineWidth',2);
+
+l4 = line(x,y4,'Color','b','LineStyle','-','LineWidth',2);
+l5 = line(x,y5,'Color','r','LineStyle','--','LineWidth',2);
+
+
+
+rt = [2.1958;roots(f2);roots(f3);roots(f4);-2.1958];
+zero = zeros(length(rt),1);
+
+
+
+scatter(rt,zero, 500, 'Marker','.','MarkerFaceColor','k','MarkerEdgeColor','k');
+
+
+text(.1,-.5,'\Delta > 0')
+text(-1,-1,'\Delta = 0')
+text(1,1,'\Delta = 0')
+text(-1,-4,'\Delta < 0')
+text(1,4,'\Delta < 0')
+
+ 
