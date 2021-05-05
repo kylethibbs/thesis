61282fd7ac836777f194db27d9f755b12bd3ae62
kylethibbs <78558790+kylethibbs@users.noreply.github.com>
2021-05-04 22:43:58 -0400

    Add files via upload

--- test.m
+++ test.m
@@ -1,13 +1,34 @@
-timer = tic;
-roots([-2 -3 -1]);
-time = toc(timer);
+% 
+% 
+% data1 = readcell('C:\School\Endicott\MATLAB\Thesis\cubicTimeTestXL.xlsx','Sheet','times');
+% 
+% rtdata = cell2mat(data1(2:end,6));
+% cddata = cell2mat(data1(2:end,7));
+% cdsdata = cell2mat(data2(2:end,8));
+% 
+% [h1,p1,ci1,stats1] = ttest2( cddata, rtdata,'Tail','left');
+% 
+% data2 = readcell('C:\School\Endicott\MATLAB\Thesis\quadraticTimeTestXL.xlsx','Sheet','times');
+% 
+% rtqdata = cell2mat(data2(2:end,6));
+% qddata = cell2mat(data2(2:end,7));
+% qdsdata = cell2mat(data2(2:end,8));
+% 
+% [h2,p2,ci2,stats2] = ttest2( qddata,rtqdata, 'Tail','left');
 
 
-timer2 = tic;
-quadratic([-2 -3 -1]);
-time2 = toc(timer2);
+data1 = readcell('C:\School\Endicott\MATLAB\Thesis\cubicTimeTestXL.xlsx','Sheet','timesImaginary');
 
+rtdata = cell2mat(data1(2:end,6));
+cddata = cell2mat(data1(2:end,7));
+cdsdata = cell2mat(data2(2:end,8));
 
+[h1,p1,ci1,stats1] = ttest2( cddata, rtdata,'Tail','left');
 
-time
-time2
+data2 = readcell('C:\School\Endicott\MATLAB\Thesis\quadraticTimeTestXL.xlsx','Sheet','timesImaginary');
+
+rtqdata = cell2mat(data2(2:end,6));
+qddata = cell2mat(data2(2:end,7));
+qdsdata = cell2mat(data2(2:end,8));
+
+[h2,p2,ci2,stats2] = ttest2( qddata,rtqdata, 'Tail','left');
